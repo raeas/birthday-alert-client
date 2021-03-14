5b69b533cc3593156e920726cd277d5577f7dafb
Rae Schneider <rannschneid@gmail.com>
2021-03-13 21:30:30 -0700

    added description of app to home page and added Add Birthday button to home page.

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -7,8 +7,12 @@ function Home() {
   return (
     <div className='Home'>
       <h1>Welcome to Birthday Alert!</h1>
-      <p>Home Component</p>
-      <button><Link to='/dashboard' className='text-link'>Go to Dashboard</Link></button>
+      <p>Have you ever forgotten a friend's birthday?</p> 
+      <p>Have you ever had to come up with gift idea at the last minute?</p>
+      <p>Have you ever had the perfect gift idea for a friend when their birtday was still 8 months away only to forget your perfect gift idea when their birthday arose?</p>
+      <p>Birthday Alert is a birthday tracker that allows you to create gift lists for your friends' and relatives' birthdays. You can add a birthday to your "Birthday List" and then create a list of gift ideas. It is easy to get started! Simply click "Add Birthday" to add a person to your Birthday List. To see upcoming birtdays on your Birthday List, click Upcoming B-Days to view details and gift lists. </p>
+      <button><Link to='/dashboard' className='text-link'>Upcoming B-Days</Link></button>
+      <button><Link to='/add-person' className='text-link'>Add Birthday</Link></button>
     </div>
   );
 }

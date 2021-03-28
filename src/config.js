e6592eca52f6e3c4284ef74a60b877b3ab86a95c
Rae Schneider <rannschneid@gmail.com>
2021-03-28 17:20:15 -0600

    port added

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  PORT: process.env.PORT || 8000,
   API_KEY: process.env.REACT_APP_API_KEY,
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
   "http://localhost:8000/api"

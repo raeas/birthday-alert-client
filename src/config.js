10163765958c9e6be27cfffa67d5ef2b1a4ee3b1
Rae Schneider <rannschneid@gmail.com>
2021-03-28 18:22:23 -0600

    updated config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  PORT: process.env.PORT || 8000,
   API_KEY: process.env.REACT_APP_API_KEY,
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
   "http://localhost:8000/api"

5d70fc14385c59889a931674a06de2a68991838a
Rae Schneider <rannschneid@gmail.com>
2021-04-03 10:51:16 -0600

    favicon

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from '../src/App/App';
+import Favicon from 'react-favicon';
 
 ReactDOM.render(
   <BrowserRouter>
+    <Favicon url="../public/favicon.ico"/>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
